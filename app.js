5114d41ba6e5edd7b46c139944d9228e10db894a
Anand Roy <itsanand.roy11@gmail.com>
2022-07-02 22:03:48 +0530

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let number = document.querySelectorAll(".number");
+let number = document.querySelectorAll('.number');
 let counter = 0;
 setInterval(() => {
   if (counter == 65) {
@@ -11,34 +11,34 @@ setInterval(() => {
 
 // Debojyoti's code
 
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Element', 'Density', { role: 'style' }],
+    ['Copper', 8.94, '#b87333'],
+    ['Silver', 10.49, 'silver'],
+    ['Gold', 19.3, 'gold'],
+    ['Platinum', 21.45, 'color: #e5e4e2'],
+  ]);
 
+  var view = new google.visualization.DataView(data);
+  view.setColumns([
+    0,
+    1,
+    { calc: 'stringify', sourceColumn: 1, type: 'string', role: 'annotation' },
+    2,
+  ]);
 
-google.charts.load("current", {packages:['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ["Element", "Density", { role: "style" } ],
-        ["Copper", 8.94, "#b87333"],
-        ["Silver", 10.49, "silver"],
-        ["Gold", 19.30, "gold"],
-        ["Platinum", 21.45, "color: #e5e4e2"]
-      ]);
-
-      var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
-
-      var options = {
-        title: "Density of Precious Metals, in g/cm^3",
-        width: 600,
-        height: 400,
-        bar: {groupWidth: "95%"},
-        legend: { position: "none" },
-      };
-      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-      chart.draw(view, options);
-  }
+  var options = {
+    title: 'Density of Precious Metals, in g/cm^3',
+    width: 600,
+    height: 400,
+    bar: { groupWidth: '95%' },
+    legend: { position: 'none' },
+  };
+  var chart = new google.visualization.ColumnChart(
+    document.getElementById('columnchart_values')
+  );
+  chart.draw(view, options);
+}
