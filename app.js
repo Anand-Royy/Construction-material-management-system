1c7ba2d10c6ac4506a18fcc3cc55639322077198
Anand Roy <itsanand.roy11@gmail.com>
2022-07-02 17:12:14 +0530

    02-07-22

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let number = document.getElementById('number');
+let number = document.querySelectorAll(".number");
 let counter = 0;
 setInterval(() => {
   if (counter == 65) {
@@ -7,44 +7,38 @@ setInterval(() => {
     counter += 1;
     number.innerHTML = counter + '%';
   }
-},30);
-
-
-
-
+}, 30);
 
 // Debojyoti's code
 
+google.charts.load('current', { packages: ['bar'] });
+google.charts.setOnLoadCallback(drawStuff);
 
+function drawStuff() {
+  var data = new google.visualization.arrayToDataTable([
+    ['Year', 'Number of Projects'],
+    ['2022', 95],
+    ['2021', 31],
+    ['2018', 12],
+    ['2017', 15],
+    ['2016', 3],
+  ]);
 
-google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawStuff);
+  var options = {
+    // title: 'Chess opening moves',
+    width: 900,
+    legend: { position: 'none' },
+    // chart: { title: 'Chess opening moves',
+    //          subtitle: 'popularity by percentage' },
+    bars: 'horizontal', // Required for Material Bar Charts.
+    axes: {
+      x: {
+        0: { side: 'top', label: 'Number of Projects' }, // Top x-axis.
+      },
+    },
+    bar: { groupWidth: '90%' },
+  };
 
-      function drawStuff() {
-        var data = new google.visualization.arrayToDataTable([
-          ['Year', 'Number of Projects'],
-          ["2022", 95],
-          ["2021", 31],
-          ["2018", 12],
-          ["2017", 15],
-          ['2016', 3]
-        ]);
-
-        var options = {
-          // title: 'Chess opening moves',
-          width: 900,
-          legend: { position: 'none' },
-          // chart: { title: 'Chess opening moves',
-          //          subtitle: 'popularity by percentage' },
-          bars: 'horizontal', // Required for Material Bar Charts.
-          axes: {
-            x: {
-              0: { side: 'top', label: 'Number of Projects'} // Top x-axis.
-            }
-          },
-          bar: { groupWidth: "90%" }
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
-        chart.draw(data, options);
-      };
+  var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+  chart.draw(data, options);
+}
